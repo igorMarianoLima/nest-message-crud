3a5d9a2bb59da8ba7f9bfdc581c9e6d83660ba5d
Igor José Castro Mariano Lima <igormarianolima05@gmail.com>
2026-01-05 22:21:43 -0300

    Feat: Configure typeORM connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,21 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MessageModule } from './message/message.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [MessageModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'db',
+      database: 'message_crud',
+      username: 'postgres',
+      password: 'postgres',
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+    MessageModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
