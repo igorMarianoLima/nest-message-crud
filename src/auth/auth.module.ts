eeac140dd703c625e13e43bc5305268909344b45
Igor José Castro Mariano Lima <igormarianolima05@gmail.com>
2026-01-09 20:54:07 -0300

    Feat: Create initial 'auth' controller and service

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,6 +2,8 @@ import { Global, Module } from '@nestjs/common';
 
 import { HashingService } from './hashing/hashing.service';
 import { BcryptService } from './hashing/bcrypt.service';
+import { AuthController } from './auth.controller';
+import { AuthService } from './auth.service';
 
 @Global()
 @Module({
@@ -10,7 +12,9 @@ import { BcryptService } from './hashing/bcrypt.service';
       provide: HashingService,
       useClass: BcryptService,
     },
+    AuthService,
   ],
   exports: [HashingService],
+  controllers: [AuthController],
 })
 export class AuthModule {}
